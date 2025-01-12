61549d71631c966ee19307f4ea4a16fb82f77102
Jason Wilczak <jason.wilczak@gmail.com>
2025-01-12 12:07:42 -0500

    added a simple file and details in readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # python-fun
+
+Super basic example of executing a v3.13+ Python file in the command line
+
+1. Make sure you install Python (i used v3.13 from here: https://www.python.org/ftp/python/3.13.1/python-3.13.1-amd64.exe)
+    a. On the first page, make sure you "add to the path" option also
+2. Open Visual Studio Code and open a folder 
+3. Create or copy the `sample.py` file and vs code should ask to install the Python tools (do it)
+4. Once the python code is in the file, make sure its saved
+5. Open the command line for vs code (ctrl+shift+` OR use the tool menu at the top)
+6. In the command line, make sure you are in the right folder
+7. type in `python sample.py -h` to see how to use the function (this is basically what line 10+ does in the file)
+8. You can run it with something like: `python sample.py Jason 25` to see the output
+9. You can make more python files and just reference them if you want to try on your own
---
+++ sample.py
@@ -0,0 +1,23 @@
+# sample.py
+
+# allows you to parse command line arguments (the 'name' input in this case)
+import argparse
+# allows you to use math functions (use cmath for comnplex numbers)
+import math
+
+# function that takes in a name and prints a greeting
+# there are lots of ways to handle arguments, you can find more info here: https://www.w3schools.com/python/python_functions.asp 
+def greet(name, age):
+    print(f"Hello, {name}!")
+    if (math.isnan(age)): print(f"You didn't enter a valid age")
+    else: print(f"You are {age} years old")
+
+# if you want to add more math functions, you can find them here: https://docs.python.org/3/library/math.html 
+
+# this validates you are runninpytg from the command line and not part of sonme other execution
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Greet someone")
+    parser.add_argument("name", type=str, help="The name of the person to greet")
+    parser.add_argument("age", type=int, help="The age of the person to greet")
+    args = parser.parse_args()
+    greet(args.name,args.age)
