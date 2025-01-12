21c2e0a3a39f69d9870f2d2c1aebc6c54e13975d
Jason Wilczak <jason.wilczak@gmail.com>
2025-01-12 12:13:19 -0500

    added a couple of helper comments

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@ import math
 
 # function that takes in a name and prints a greeting
 # there are lots of ways to handle arguments, you can find more info here: https://www.w3schools.com/python/python_functions.asp 
+# make sure do defensive coding in your function, check if number, numnber boundaries, etc
 def greet(name, age):
     print(f"Hello, {name}!")
     if (math.isnan(age)): print(f"You didn't enter a valid age")
@@ -15,6 +16,7 @@ def greet(name, age):
 # if you want to add more math functions, you can find them here: https://docs.python.org/3/library/math.html 
 
 # this validates you are runninpytg from the command line and not part of sonme other execution
+# if you want to modify the add_argument, you can find more here: https://docs.python.org/3/library/argparse.html 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Greet someone")
     parser.add_argument("name", type=str, help="The name of the person to greet")
